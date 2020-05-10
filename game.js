e8cca243a1fba002b0dde2a3f9197f0f561cad81
normalsled <appliedboredom@gmail.com>
2020-05-09 19:51:29 -0700

    some more backend

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+//this head section has all our definitions.
+
 var gameState = {
     currentQuestion: 0,
     correctAnswers: 0,
@@ -13,8 +15,10 @@ var questions = [
             c: "1900",
             d: "1952"
         },
-        correctAnswer: this.answers.c,
-        playerAnswer:,
+        correctAnswer: function (){
+            return this.answers.c
+        },
+        playerAnswer: checkAnswer(),
         questionGif: "anglo-persian.jpg"
     },
 
@@ -23,14 +27,15 @@ var questions = [
         answers: {
             a: "Tehran",
             b: "Abadan",
-            c: "",
-            d: ""
+            c: "Shiraz",
+            d: "Tabriz"
         },
-        correctAnswer: this.answers.c,
+        correctAnswer: function (){
+            return this.answers.b
+        },
         playerAnswer: null,
         isCorrect: checkAnswer(),
         questionGif: "anglo-persian.jpg"
-
     },
 
     // questionThree = {
@@ -68,51 +73,72 @@ var questions = [
 
 
 function startGame() {
-    //change current screen to first question
-    newQuestion(questions[gameState.currentQuestion])
+    //if the start game or new game button is clicked, reset gamestate's current question and correct answers values, then change current screen to first question
+    gameState.currentQuestion = 0,
+    gameState.correctAnswers = 0,
+    console.log(newQuestion());
+
 };
 
 
-function newQuestion (question) {
-    //display a question on the screen
+function newQuestion () {
+    //when it's time to show a new question, display the current question on the screen in the question box, start the timer, and display all the question's answers in the answer box.
+    return (questions[gameState.currentQuestion])
 };
 
-function getAnswer(question) {
-    //when the player clicks an answer, animate the answer they clicked, and record the answer in the question object as this.playerAnswer
+function getAnswer() {
+    //when the player clicks an answer, animate the answer they clicked, and record the answer in the question object as this. If the timer ticks down to zero, record the player's answer as null.
 
-    // var answer = $(this)
-    // return answer
 };
 
-function checkAnswer(answer,correctAnswer){
+function checkAnswer(answer, correctAnswer){
+    //if the player's answer matches the correct answer of the current question, set that question's isCorrect property to true.
     console.log(answer, correctAnswer);
     if (answer == correctAnswer){
-        return true
+        gameState.currentQuestion.isCorrect = true
+        trackState();
     }
     else 
-    return false;
+        gameState.currentQuestion.isCorrect = false
+        trackState();
+    return;
 };
 
 
-function trackState (question) {
-    //when the player answers a question, record their answer.
-    if (question.isCorrect === true){
+function trackState () {
+    //when the player answers a question, record their answer. if it's correct, add a correct answer to the game state's correct answer count and advance to the next question in the question array. If it is incorrect, don't add anything, but advance to the next question.
+
+    if (questions[gameState.currentQuestion].isCorrect == true){
         gameState.correctAnswers ++;
         gameState.currentQuestion ++;
         checkGameOver();
     }
     else
-    gameState.currentQuestion ++;
-    checkGameOver();
+        gameState.currentQuestion ++;
+        checkGameOver();
     return;
 };
 
-function checkGameOver(gameState){
+function checkGameOver(){
     //if we're at the end of our list of questions, end the game.
     if (gameState.currentQuestion > questions.length) {
-        gameOver()
+        gameOver();
     }
     else
-    newQuestion()
-}
+        newQuestion();
+};
 
+function gameOver (){
+    //increment the games played counter in game state, and reset all questions' is correct properties to false in case we want to play again. that way, track state won't log the wrong count.
+    gameState.gamesPlayed ++;
+    questions.forEach(question => {
+        question.isCorrect = false
+    });
+    //show game over HTML screen in viewport
+};
+
+
+
+//ok here's our main code flow.
+
+startGame();
